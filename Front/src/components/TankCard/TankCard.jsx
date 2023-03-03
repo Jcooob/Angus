876734b3cc0f265b554b92940d079f55e7b6a8ee
Jacobo Renato Cohello Revolledo <j-acobo@hotmail.com>
2023-03-03 11:29:25 -0500

    first commit

--- Front/src/components/TankCard/TankCard.jsx
+++ Front/src/components/TankCard/TankCard.jsx
@@ -1,6 +1,8 @@
+import "./TankCard.modules.css"
 import { Link } from "react-router-dom";
 import { tankTypeName, tankNationName, convertToRoman } from "../Functions/functions";
-const backGroundImg = "https://na-wotp.wgcdn.co/static/5.117.1_a28195/wotp_static/img/tankopedia_new/frontend/scss/tankopedia-detail/img/hangar-bg.webp"
+const backGroundImg = "https://na-wotp.wgcdn.co/static/5.117.1_a28195/wotp_static/img/tankopedia_new/frontend/scss/tankopedia-detail/img/hangar-bg.webp";
+
 
 const TankCard = ({ tank_id, type, short_name, nation, tier, image }) => {
 
